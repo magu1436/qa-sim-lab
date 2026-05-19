fcd8ec54410e808853d44cdb69624822cdca60e1
magu1436 <hfzoro77@gmail.com>
2026-05-19 18:23:24 +0900

    feat: implement a customized dialog

---
+++ frontend/src/component/Dialog.tsx
@@ -0,0 +1,37 @@
+import type { FC } from "react"
+
+import { Button, DialogActions, DialogContent, DialogTitle, Dialog as MUIDialog } from "@mui/material"
+
+type ActionsProps = {
+    submitButtonlabel: string,
+    cancelButtonLabel: string,
+    onSubmit?: () => void,
+    onCancel?: () => void,
+}
+
+type DialogProps = {
+    open: boolean,
+    onClose: () => void,
+    title: string,
+    children: React.ReactNode[],
+    actions?: ActionsProps,
+}
+
+const Dialog: FC<DialogProps> = ({ open, onClose, title, children, actions }) => {
+    return (
+        <MUIDialog open={open} onClose={onClose}>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogContent dividers>
+                {children}
+            </DialogContent>
+            {actions && (
+                <DialogActions>
+                    <Button onClick={actions.onCancel}>{actions.cancelButtonLabel}</Button>
+                    <Button type="submit" onClick={actions.onSubmit}>{actions.submitButtonlabel}</Button>
+                </DialogActions>
+            )}
+        </MUIDialog>
+    );
+};
+
+export default Dialog;
