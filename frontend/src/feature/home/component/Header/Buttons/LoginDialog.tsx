30fa00c40e03a13ac3b0397e26d3872de0b83db1
magu1436 <hfzoro77@gmail.com>
2026-05-19 22:04:32 +0900

    feat: show TextField in error state when validation fails in the dialog

--- frontend/src/feature/home/component/Header/Buttons/LoginDialog.tsx
+++ frontend/src/feature/home/component/Header/Buttons/LoginDialog.tsx
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material";
+import { Stack, TextField } from "@mui/material";
 import { useCallback, useEffect, useState, type FC } from "react";
 import Dialog from "@/component/Dialog";
 import PasswordTextField from "@/component/PasswordTextField";
@@ -9,18 +9,58 @@ import NotGivenStudentIdError from "@/feature/user/error/login/NotGivenStudentId
 import useLoginMutation from "@/feature/user/hook/login";
 
 type LoginDialogProps = {
-    open: boolean,
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>,
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const LoginDialog: FC<LoginDialogProps> = ({ open, setOpen }) => {
-
   const [studentId, setStudentId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [studentIdHelperText, setStudentIdHelperText] = useState<string>();
   const [passwordHelperText, setPasswordHelperText] = useState<string>();
 
   const mutation = useLoginMutation();
+
+  const handleStudentIdChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setStudentId(e.target.value);
+      setStudentIdHelperText(undefined);
+    },
+    [setStudentId, setStudentIdHelperText],
+  );
+
+  const handlePasswordChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+      setPasswordHelperText(undefined);
+    },
+    [setPassword, setPasswordHelperText],
+  );
+
+  const handleSubmit = useCallback(() => {
+    if (mutation.isPending) return;
+    let isError = false;
+    if (studentId === "") {
+      setStudentIdHelperText("学籍番号は必須です。");
+      isError = true;
+    }
+    if (password === "") {
+      setPasswordHelperText("パスワードは必須です。");
+      isError = true;
+    }
+    if (isError) return;
+    const form = {
+      student_id: studentId,
+      password,
+    };
+    mutation.mutate(form);
+  }, [mutation, studentId, password]);
+
+  const handleCancel = useCallback(() => {
+    setOpen(false);
+    setStudentId("");
+    setPassword("");
+  }, [setOpen, setStudentId, setPassword]);
 
   useEffect(() => {
     if (mutation.isError) {
@@ -39,25 +79,11 @@ const LoginDialog: FC<LoginDialogProps> = ({ open, setOpen }) => {
       }
       return;
     }
-    setStudentIdHelperText(undefined);
-    setPasswordHelperText(undefined);
+    if (mutation.isSuccess) {
+      handleCancel();
+      mutation.reset();
+    }
   }, [mutation, setStudentIdHelperText, setPasswordHelperText]);
-
-  const handleStudentIdChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setStudentId(e.target.value);
-      setStudentIdHelperText(undefined);
-    },
-    [setStudentId, setStudentIdHelperText],
-  );
-
-  const handlePasswordChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setPassword(e.target.value);
-      setPasswordHelperText(undefined);
-    },
-    [setPassword, setPasswordHelperText],
-  );
 
   return (
     <Dialog
@@ -67,26 +93,30 @@ const LoginDialog: FC<LoginDialogProps> = ({ open, setOpen }) => {
       actions={{
         submitButtonlabel: "ログイン",
         cancelButtonLabel: "キャンセル",
-        onSubmit: () => setOpen(false),
-        onCancel: () => setOpen(false),
+        onSubmit: handleSubmit,
+        onCancel: handleCancel,
       }}
     >
-      <TextField
-        label="学籍番号"
-        variant={mutation.isPending ? "filled" : "outlined"}
-        value={studentId}
-        placeholder="xDIMxxxx"
-        onChange={handleStudentIdChange}
-        helperText={studentIdHelperText}
-      />
-      <PasswordTextField
-        label="パスワード"
-        variant={mutation.isPending ? "filled" : "outlined"}
-        password={password}
-        placeHolder="password"
-        onChange={handlePasswordChange}
-        helperText={passwordHelperText}
-      />
+      <Stack spacing={2} direction={"column"}>
+        <TextField
+          label="学籍番号"
+          variant={mutation.isPending ? "filled" : "outlined"}
+          value={studentId}
+          placeholder="xDIMxxxx"
+          onChange={handleStudentIdChange}
+          helperText={studentIdHelperText}
+          error={studentIdHelperText !== undefined}
+        />
+        <PasswordTextField
+          label="パスワード"
+          variant={mutation.isPending ? "filled" : "outlined"}
+          password={password}
+          placeHolder="password"
+          onChange={handlePasswordChange}
+          helperText={passwordHelperText}
+          error={passwordHelperText !== undefined}
+        />
+      </Stack>
     </Dialog>
   );
 };
--- frontend/src/feature/home/index.tsx
+++ frontend/src/feature/home/index.tsx
@@ -1,7 +1,14 @@
+import { Box } from "@mui/material";
+import Logo from "./component/Header/Logo";
+import LoginButton from "./component/Header/Buttons/LoginButton";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Box sx={{height: "30vh", width: "100%"}}>
+        <Logo />
+        <LoginButton />
+      </Box>
     </div>
   );
 };
