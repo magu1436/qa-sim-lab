b83b56af21b09c031c53584caf696f6ed1450447
magu1436 <hfzoro77@gmail.com>
2026-05-19 16:36:22 +0900

    feat: remove default starter pages and components, and add the initial screen

---
+++ frontend/src/app/routes/basic.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router";
+import Home from "../../feature/home";
+
+const basic = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+    </>,
+  ),
+);
+
+export default basic;
