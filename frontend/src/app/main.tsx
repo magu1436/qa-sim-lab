81170658f58491865f4ba7ed6358824883a86ac0
magu1436 <hfzoro77@gmail.com>
2026-05-19 17:07:15 +0900

    chore: add frontend dependencies

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import basic from "./routes/basic";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={basic} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={basic} />
+    </QueryClientProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
   </StrictMode>,
 );
